77a2fdaf9c3e0e81b98ec91b3db7e70d1449657d
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-19 23:05:42 -0400

    Wrote the main code for highlighting
    also had ai fix my imports way to mmany times

--- src/main/java/butterlabs/smartsleep/SmartSleep.java
+++ src/main/java/butterlabs/smartsleep/SmartSleep.java
@@ -1,28 +1,55 @@
 package butterlabs.smartsleep;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.block.BedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class SmartSleep implements ModInitializer {
 	public static final String MOD_ID = "smart-sleep";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
 		LOGGER.info("Hello Fabric world!");
+
 		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-			
-			return ActionResult.PASS;
+			if (!world.isClientSide()) {
+				BlockPos pos = hitResult.getBlockPos();
+				BlockState state = world.getBlockState(pos);
+
+				if (state.getBlock() instanceof BedBlock) {
+					AABB searchArea = new AABB(pos).inflate(16.0);
+					List<Monster> monsters = world.getEntitiesOfClass(
+							Monster.class,
+							searchArea,
+							mob -> mob.isAlive()
+					);
+
+					for (Monster monster : monsters) {
+						monster.addEffect(new MobEffectInstance(
+								MobEffects.GLOWING,
+								10000,
+								0
+						));
+					}
+				}
+			}
+			return InteractionResult.PASS;
 		});
 	}
 
