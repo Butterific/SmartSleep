cb278594bc2bd9b40619bc28082c81f776ba0d66
Butterific <166562975+Butterific@users.noreply.github.com>
2026-09-19 22:31:55 -0400

    Copy all the files from the fabric template generator

---
+++ src/client/java/butterlabs/smartsleep/client/SmartSleepClient.java
@@ -0,0 +1,10 @@
+package butterlabs.smartsleep.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmartSleepClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
